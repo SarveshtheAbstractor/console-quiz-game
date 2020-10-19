cf716e4d474418150ce2519213f870a0043bd4b9
SarveshtheAbstractor <hiwasesarvesh@yahoo.com>
2020-10-19 21:53:27 +0530

    added more meaning full questions to the quiz

--- app.js
+++ app.js
@@ -5,8 +5,15 @@
     this.correct = correct;
 }
 var q1 = new Question('Is javascript most popular language in the world?',['Yes','No'],0);
-var q2 = new Question('How is coding really?',['Hard','fun','tedious','boring'],1);
-var q3 = new Question('The course teacher\'s name?',['John','Rob','Jonas'],2);
+var q2 = new Question('How is coding really?',['Hard','Fun','tedious','boring'],1);
+var q3 = new Question('India\'s capital?',['New-Delhi','Delhi','Bombay','Chandigarh'],1);
+var q4 = new Question('BTS Main dancer ?',['Jin','Jungkook','Jimin','Suga','J-Hope'],4);
+var q5 = new Question('Aamir khan\'s best movie?',['Taalash','Pk','3-Idiots'],2);
+var q6 = new Question('In One Direction who is most handsome?',['Louis','Harry','Niall','Liam','Zayn'],4);
+var q7 = new Question('Which WebFrame Work is Best?',['Laravel','MEAN','MERN'],1);
+var q8 = new Question('Which is best Game of All time?',['GTA-5','The-Witcher','Red-dead Redemption','God Of War'],0);
+var q9 = new Question('Best Series of all time?',['Stranger Things','Friends','How I met Your mother','Sacred Games','Mirzapur'],3);
+var q10 = new Question('Best Hollywood Film?',['Mad-Max','The Revenant','Forest Gumpp','The Pursuit of Happiness'],2);
 
 Question.prototype.displayquestion = function(){
     console.log(this.question);
@@ -34,7 +41,7 @@ Question.prototype.checkanswer = function(ans,callback){
     
 }
                 
-var questions = [q1,q2,q3];
+var questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
 
 function score(){
     var sc = 0;
